02e9fabe910bd370fb76d14ae0aebc3f035410bc
rhsajib <sajib.mmebuet@gmail.com>
2023-06-07 21:12:33 +0600

    drf project changed

--- projects/drf/backend/firstapp/urls.py
+++ projects/drf/backend/firstapp/urls.py
@@ -6,4 +6,6 @@ from . import views
 urlpatterns = [
     path('first/', views.firstAPI, name = 'firstAPI'),
     path('registration/', views.registrationAPI, name = 'registrationAPI'),
+    path('contact/', views.ContactAPIView.as_view(), name = 'ContactAPIView'),
+    path('contactone/', views.ContactAPIViewOne.as_view(), name = 'ContactAPIViewOne'),
 ]
--- projects/drf/backend/firstapp/views.py
+++ projects/drf/backend/firstapp/views.py
@@ -66,8 +66,138 @@ def firstAPI(request):
 
 
 
-"""Class based API view"""
+"""Class based API view without serializer"""
 
 from rest_framework.views import APIView
+from .models import Contact
 
-class Contact(APIView)
+class ContactAPIView2(APIView):   
+    permission_classes = [AllowAny,]
+    def post(self, request, format=None):
+        data = request.data
+
+        name = data['name']
+        email = data['email']
+        subject = data['subject']
+        phone = data['phone']
+        details = data['details']
+
+        if Contact.objects.filter(name=name).exists():
+            return Response({'error': 'This name already exists'})
+
+        contact = Contact(
+            name = name,
+            email = email,
+            subject = subject,
+            phone = phone,
+            details = details
+        )
+        contact.save()       
+        return Response({'succeess': 'Successfully saved!!'})
+    
+    def get(self, request, format=None):
+        return Response({'success': 'Successfully saved!! from get'})
+    
+
+"""Class based API view with Modelserializer"""
+
+from rest_framework.views import APIView
+from .models import Contact
+from .serializers import ContactSerializers
+
+class ContactAPIView(APIView):   
+    permission_classes = [AllowAny,]
+    def post(self, request, format=None):
+        # load request data
+        data = request.data
+
+        # create instance
+        serializer = ContactSerializers(data=data)
+
+        # check validity
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'succeess': 'Successfully saved!!'})
+            # return Response(serializer.data) # it will return serializer data        
+        else:
+            return Response({'error': 'Name already exists'})
+
+
+    def get(self, request, format=None):
+        # get all data of the class
+        query_set = Contact.objects.all()
+        serializer = ContactSerializers(query_set, many=True) 
+        # many=True is added as query_set holds multiple objects
+
+        return Response(serializer.data)   # response will be a list of dictionaries
+
+
+        """
+        
+        #  get data of an id
+        # request get with only id
+
+        data = request.data  
+        id = data.get('id')
+        # id = data['id']
+
+        query_set = Contact.objects.get(pk=id)
+        serializer = ContactSerializers(query_set, many=False) 
+        # many=False is added as query_set holds only one object
+
+        return Response(serializer.data)    # response will be a dictionary not a list of dictionaries.
+        
+
+        """
+       
+
+"""Class based API view with only serializer"""
+
+from rest_framework.views import APIView
+from .models import Contact
+from .serializers import ContactSerializerOne
+
+class ContactAPIViewOne(APIView):   
+    permission_classes = [AllowAny,]
+    def post(self, request, format=None):
+        # load request data
+        data = request.data
+
+        # create an instance
+        serializer = ContactSerializerOne(data=data)
+
+        # check validity
+        if serializer.is_valid():
+            serializer.save()
+            # return Response({'succeess': 'Successfully saved!!',})
+            return Response(serializer.data) # it will return serializer data        
+        else:
+            return Response({'error': 'Name already exists'})
+
+
+    def get(self, request, format=None):
+        # get all data of the class
+        query_set = Contact.objects.all()
+        serializer = ContactSerializerOne(query_set, many=True) 
+        # many=True is added as query_set holds multiple objects
+
+        return Response(serializer.data)   # response will be a list of dictionaries
+
+
+        """
+        
+        #  get data of an id
+        # request get with only id
+
+        data = request.data  
+        id = data.get('id')
+        # id = data['id']
+
+        query_set = Contact.objects.get(pk=id)
+        serializer = ContactSerializerOne(query_set, many=False) 
+        # many=False is added as query_set holds only one object
+
+        return Response(serializer.data)    # response will be a dictionary not a list of dictionaries.
+        
+
+        """
