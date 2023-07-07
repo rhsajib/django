3c8703851d5eaeb0f1d2aaf3fbb806ad04136202
Md. Rashadul Hoq Sajib <111883695+rhsajib@users.noreply.github.com>
2023-07-08 02:33:59 +0600

    Update info.py

--- projects/event-management-and-ticketing-system/backend/emts/info.py
+++ projects/event-management-and-ticketing-system/backend/emts/info.py
@@ -2,7 +2,7 @@
 
 EMAIL_USE_TLS = True                             # Enable TLS encryption
 EMAIL_HOST = 'smtp.gmail.com'                    # Replace with your SMTP server host
-EMAIL_HOST_USER = 'rhsajib15@gmail.com'          # Replace with your email address
+EMAIL_HOST_USER = 'example@gmail.com'          # Replace with your email address
 EMAIL_HOST_PASSWORD = 'password'                 # Replace with your email password
 EMAIL_PORT = 587                                 # Replace with the SMTP server port
 
